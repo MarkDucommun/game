ac3c7c9a55aa169b38e3d9c14b23115a0519785b
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-20 13:15:46 -0500

    beginning OO structure

--- public/js/application.js
+++ public/js/application.js
@@ -1,40 +1,66 @@
 $(document).ready(function() {
-
+  var one = new Box(0, 0, 'red')
+  one.paint()
 });
 
-function Position(x, y){
-  this.set_x(x)
-  this.set_y(y)
-}
-
-Position.method('setX', function(x){
-  this.x = x
-  return x
-})
-
-Position.method('setY', function(y){
-  this.y = y
-  return y
-})
-
-Position.method('getX', function(){
-  return this.x
-})
-
-Position.method('getY', function(){
-  return this.y
-})
-
-function Box(x, y, color){
+function Position(x, y, color){
   this.setX(x)
   this.setY(y)
   this.setColor(color)
 }
 
-Box.inherits(Position)
+Position.prototype = {
+  setX : function(x){
+    this.x = x
+    return x
+  },
 
-Box.method('setColor', function(color){
-  this.color = color
-  return this.color
+  setY : function(y){
+    this.y = y
+    return y
+  },
+
+  setColor : function(color){
+    this.color = color
+    return this.color
+  },
+  
+  getX : function(){
+    return this.x
+  },
+
+  getY : function(){
+    return this.y
+  },
+
+  getColor : function(color){
+    return this.color
+  },
+
+  move : function(x, y){
+  },
+
+  paint : function(){
+    var square = $('#sq' + (this.getY() * 4 + this.getX()))
+    square.css('background', this.getColor())
+  }
+}
+
+function Box(x, y, color){
+  Position.call(this, x, y, color)
+}
+
+Box.prototype = Object.create(Position.prototype, {
+  move : function(x, y){
+    this.setX(x)
+    this.setY(y)
+    this.paint()
+  }
 })
+
+function Obstacle(x, y, color){
+  this.position = new Position(x, y, color)
+}
+
+
 
